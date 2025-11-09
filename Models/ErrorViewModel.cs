826edc6041e63655603556626030ff46380a2e58
amadorel <elamador05@gmail.com>
2025-11-08 21:35:14 -0500

    Creation of repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LibsterFinalProj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
