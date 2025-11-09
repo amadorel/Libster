64128c0754c0b3b46f1dff53fff773642b49fad2
amadorel <elamador05@gmail.com>
2025-11-08 22:33:56 -0500

    Add .gitignore file to project; removed tracked changes for bin/ & obj/ folders

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+/*
+    site.js
+            Author:         Eli Amador
+            Creation Date:  2025-11-08
+            Last revised:   2024-11-XX 
+            Description:    JS scripting for global Libster site 
+*/
 
-// Write your JavaScript code.
+
+const fontUp = document.getElementById("fontUp"); 
+      fontDown = document.getElementById("fontDown");
+      //selector to get text on page; this is house in main afaik 
+      // font = document.getElementById();
+
+      //Use to manage font increase and decrease
+      //fontUp.addEventListener(); 
+      //fotnDown.addEventListener(); 
